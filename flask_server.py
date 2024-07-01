7f8c931b85f9d234958dc26752d318041d976064
DefinatlyNotAI <Nirt_12023@outlook.com>
2024-07-01 07:23:23 +0300

    Created Test Generator - Sample for v1.0.1 for Online API usage - Only a commit for testing

--- flask_server.py
+++ flask_server.py
@@ -85,4 +85,4 @@ def upload_file():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=False)
+    app.run(host='127.0.0.1', port=int(os.environ.get("PORT", 5000)), debug=False)
